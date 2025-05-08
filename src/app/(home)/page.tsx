0f607eba0927703f81475d4f3696e4880ed79b0d
Raj Roy <rajroy894290@gmail.com>
2025-05-08 11:55:48 +0530

    adding documents search functionality and documents are shown only to their respective owners.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,8 +6,15 @@ import { TemplatesGallery } from "./templates-gallery";
 import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { DocumentsTable } from "./documents-table";
+import { useSearchParams } from "next/navigation";
+import { useSearchParam } from "@/hooks/use-search-param";
 const Home = () => {
-  const { results, status, loadMore } = usePaginatedQuery(api.documents.getDocuments, {}, { initialNumItems: 5 });
+  const [search] = useSearchParam();
+  const {
+    results,
+    status,
+    loadMore,
+  } = usePaginatedQuery(api.documents.getDocuments, { search }, { initialNumItems: 5 });
   return (
     <div className="flex flex-col min-h-screen">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
